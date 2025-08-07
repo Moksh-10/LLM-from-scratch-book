8bf14520bb84379179f8c053ae31e0359fafe5c8
Moksh-10 <agmoksh10@gmail.com>
2025-08-07 23:45:03 +0530

    W

--- ch05.py
+++ ch05.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import torch
+from torch import nn
 from tqdm import tqdm
 
 
@@ -34,6 +35,24 @@ def gen(model, idx, max_new_tok, context_size, temp=0.0, top_k=None, eos_id=None
         idx = torch.cat((idx, idx_next), dim=1)
 
     return idx
+
+
+def text_to_token_ids(text, tokenizer):
+    enc = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+    enc_ten = torch.tensor(enc).unsqueeze(0)
+    return enc_ten
+
+
+def token_ids_tok_text(token_ids, tokenizer):
+    dec = token_ids.squeeze(0)
+    return tokenizer.decode(dec.tolist())
+
+
+def calc_loss_batch(inp_batch, tar_batch, model, device):
+    inp_batch, tar_batch = inp_batch.to(device), tar_batch.to(device)
+    logits = model(inp_batch)
+    loss = nn.functional.cross_entropy(logits.flatten(0, 1), tar_batch.flatten())
+    return loss
 
 
 
