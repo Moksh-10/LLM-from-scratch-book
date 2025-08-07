d68d2071ace0f0ba6d2c065516e615564c9eee9d
Moksh-10 <agmoksh10@gmail.com>
2025-08-07 20:44:29 +0530

    fcuk

--- ch05.py
+++ ch05.py
@@ -36,42 +36,4 @@ def gen(model, idx, max_new_tok, context_size, temp=0.0, top_k=None, eos_id=None
     return idx
 
 
-def train(model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter, start_context, tokenizer):
-    trian_losses, val_losses, track_tokens_seen = [], [], []
-    tokens_seen, global_step = 0, -1
-
-    for epoch in range(num_epochs):
-        model.train()
-
-        for inp_batch, tar_batch in train_loader:
-            optimizer.zero_grad()
-            loss = loss_batch(inp_batch, tar_batch, model, device)
-            loss.backward()
-            optimizer.step()
-            tokens_seen += inp_batch.numel()
-            global_step += 1
-
-            if global_step % eval_freq == 0:
-                train_loss, val_loss = eval_model(
-                    model, train_loader, val_loader, device, eval_iter
-                )
-                trian_losses.append(train_loss)
-                val_losses.append(val_loss)
-                track_tokens_seen.append(tokens_seen)
-                print(f"Epoch: {epoch + 1} (Step {global_step:06g}): "
-                      f"Train loss: {train_loss:.3f}, Val loss: {val_loss:.3f}")
-
-        gen_and_print(model, tokenizer, device, start_context)
-
-    return trian_losses, val_losses, track_tokens_seen
-
-
-def eval_model(model, train_loader, val_loader, device, eval_iter):
-    model.eval()
-    with torch.no_grad():
-        train_loss = loss_loader(train_loader, model, device, num_batches=eval_iter)
-        val_loss = loss_loader(val_loader, model, device, num_batches=eval_iter)
-    model.train()
-    return train_loss, val_loss
-
 
